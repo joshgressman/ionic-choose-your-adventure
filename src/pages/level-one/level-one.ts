8b9cda0111cdec822d386420339d97627917ef85
joshgressman <joshgressman@gmail.com>
2017-11-13 18:51:00 -0600

    added level one page

---
+++ src/pages/level-one/level-one.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-level-one',
+  templateUrl: 'level-one.html',
+})
+export class LevelOnePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+
+}
